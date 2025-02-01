60b47f4b00f23b642af29ab08626870f7b0a46cc
andj712 <andj712@gmail.com>
2025-02-01 20:15:40 +0100

    creating database

---
+++ DiaryApp/Controllers/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using DiaryApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiaryApp.Controllers.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options) {
+        
+
+
+        }   
+
+        public DbSet<DiaryEntry> DiaryEntries { get; set; } 
+        
+    }
+}
