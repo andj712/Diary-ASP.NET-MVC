63cee67ad015e4f18aadfd03356df5a72e7a6a4e
andj712 <andj712@gmail.com>
2025-02-02 19:27:50 +0100

    table created

--- DiaryApp/Controllers/DiaryEntriesController.cs
+++ DiaryApp/Controllers/DiaryEntriesController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DiaryApp.Controllers.Data;
+using DiaryApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DiaryApp.Controllers
 {
     public class DiaryEntriesController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public DiaryEntriesController(ApplicationDbContext db)
+        {
+            _db=db;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<DiaryEntry> objDairyEntryList=_db.DiaryEntries.ToList(); 
+            return View(objDairyEntryList);
         }
     }
 }
