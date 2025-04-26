f2cb63e8478dde719446d4c393f6b3d7b6257f10
andj712 <andj712@gmail.com>
2025-04-26 19:58:24 +0200

    git

--- DiaryApp/Controllers/DiaryEntriesController.cs
+++ DiaryApp/Controllers/DiaryEntriesController.cs
@@ -40,9 +40,69 @@ namespace DiaryApp.Controllers
             return View(obj);
         }
 
+        [HttpGet] //ovo je po defaultu httpget nije moralo da se napise
         public IActionResult Edit(int id)
         {
-            return View();
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            DiaryEntry? diaryEntry = _db.DiaryEntries.Find(id);
+            if (diaryEntry == null)
+            {
+                return NotFound();
+            }
+            
+            return View(diaryEntry);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(DiaryEntry obj)
+        {
+            if (obj != null && obj.Title.Length < 3)
+            {
+                ModelState.AddModelError("Title", "Title too short");
+            }
+
+            if (ModelState.IsValid)
+            {
+
+                _db.DiaryEntries.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
+
+        [HttpGet] //ovo je po defaultu httpget nije moralo da se napise
+        public IActionResult Delete(int id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            DiaryEntry? diaryEntry = _db.DiaryEntries.Find(id);
+            if (diaryEntry == null)
+            {
+                return NotFound();
+            }
+
+            return View(diaryEntry);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(DiaryEntry obj)
+        {
+            
+
+           
+
+            _db.DiaryEntries.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+            
+
         }
     }
 }
